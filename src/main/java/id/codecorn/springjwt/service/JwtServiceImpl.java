5bfd09dd6c72f03d43fad09b1922d8ab8c46a3f3
hermandev <hermantolakoro@gmail.com>
2023-01-11 05:37:53 +0800

    fix: add extrAllClaims

--- src/main/java/id/codecorn/springjwt/service/JwtServiceImpl.java
+++ src/main/java/id/codecorn/springjwt/service/JwtServiceImpl.java
@@ -1,13 +1,35 @@
 package id.codecorn.springjwt.service;
 
+import java.security.Key;
+
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtServiceImpl implements JwtService {
+    private static final String SECRET_KEY = "2F413F4428472B4B6250655368566B5970337336763979244226452948404D63";
 
     @Override
     public String extractUsername(String jwt) {
         return null;
+    }
+
+    private Claims extrAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 }
