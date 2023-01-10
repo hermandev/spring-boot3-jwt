90f3b3352821df46dc99dae3273fb9abe0ae50a1
hermandev <hermantolakoro@gmail.com>
2023-01-11 05:24:03 +0800

    feat(entity): add roles entity

---
+++ src/main/java/id/codecorn/springjwt/entity/Roles.java
@@ -0,0 +1,18 @@
+package id.codecorn.springjwt.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "t_roles")
+public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
