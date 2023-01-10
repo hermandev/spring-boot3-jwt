df6743b2a65e02d533b3825321a665e09534e316
hermandev <hermantolakoro@gmail.com>
2023-01-10 15:29:07 +0800

    initial project

---
+++ src/test/java/id/codecorn/springjwt/SpringBoot3JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package id.codecorn.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
