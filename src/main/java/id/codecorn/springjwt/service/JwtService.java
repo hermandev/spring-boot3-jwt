a46590e1e4b1b86eb8fe049f703f0e7897919580
hermandev <hermantolakoro@gmail.com>
2023-01-11 05:46:51 +0800

    fix: add generateToken

--- src/main/java/id/codecorn/springjwt/service/JwtService.java
+++ src/main/java/id/codecorn/springjwt/service/JwtService.java
@@ -1,13 +1,18 @@
 package id.codecorn.springjwt.service;
 
+import java.util.Map;
 import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
 
 import io.jsonwebtoken.Claims;
 
 public interface JwtService {
 
-    String extractUsername(String jwt);
+    String extractUsername(String token);
 
     <T> T extractClaim(String token, Function<Claims, T> claimsResolver);
+
+    String generateToken(Map<String, Object> extractClaims, UserDetails userDetails);
 
 }
--- src/main/java/id/codecorn/springjwt/service/JwtServiceImpl.java
+++ src/main/java/id/codecorn/springjwt/service/JwtServiceImpl.java
@@ -1,12 +1,16 @@
 package id.codecorn.springjwt.service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -15,8 +19,8 @@ public class JwtServiceImpl implements JwtService {
     private static final String SECRET_KEY = "2F413F4428472B4B6250655368566B5970337336763979244226452948404D63";
 
     @Override
-    public String extractUsername(String jwt) {
-        return null;
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
     }
 
     @Override
@@ -37,6 +41,18 @@ public class JwtServiceImpl implements JwtService {
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    @Override
+    public String generateToken(Map<String, Object> extractClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
 }
