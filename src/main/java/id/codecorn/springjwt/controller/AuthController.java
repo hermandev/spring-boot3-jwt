f3d76c3d91d01c350c5b92c209306ed3d46e72c9
hermandev <hermantolakoro@gmail.com>
2023-01-11 19:17:42 +0800

    fix(controller): add register controller

--- src/main/java/id/codecorn/springjwt/controller/AuthController.java
+++ src/main/java/id/codecorn/springjwt/controller/AuthController.java
@@ -8,13 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import id.codecorn.springjwt.dto.AuthRequest;
 import id.codecorn.springjwt.dto.AuthResponse;
+import id.codecorn.springjwt.dto.RegisterRequest;
+import id.codecorn.springjwt.service.AuthService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest req) {
-        return null;
+        return ResponseEntity.ok(authService.login(req));
+    }
+
+    @PostMapping("/register")
+    public void register(@RequestBody RegisterRequest req) {
+        authService.register(req);
     }
 }
