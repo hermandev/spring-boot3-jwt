5baa65124b8ccf24e9020ba10535444b5ef90940
hermandev <hermantolakoro@gmail.com>
2023-01-11 19:18:00 +0800

    fix(dto): add builder to auth response

--- src/main/java/id/codecorn/springjwt/dto/AuthResponse.java
+++ src/main/java/id/codecorn/springjwt/dto/AuthResponse.java
@@ -1,8 +1,14 @@
 package id.codecorn.springjwt.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
     private String token;
 }
