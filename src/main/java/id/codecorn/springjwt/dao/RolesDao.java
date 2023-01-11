eb0c6001a0a617dd3766ff0251b2368470a22df6
hermandev <hermantolakoro@gmail.com>
2023-01-11 18:44:23 +0800

    fix(repository): update repository role and user

--- src/main/java/id/codecorn/springjwt/dao/RolesDao.java
+++ src/main/java/id/codecorn/springjwt/dao/RolesDao.java
@@ -1,9 +1,11 @@
 package id.codecorn.springjwt.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import id.codecorn.springjwt.entity.Roles;
 
+@Repository
 public interface RolesDao extends JpaRepository<Roles, Long> {
 
 }
--- src/main/java/id/codecorn/springjwt/dao/UsersDao.java
+++ src/main/java/id/codecorn/springjwt/dao/UsersDao.java
@@ -3,9 +3,11 @@ package id.codecorn.springjwt.dao;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import id.codecorn.springjwt.entity.Users;
 
+@Repository
 public interface UsersDao extends JpaRepository<Users, String> {
     Optional<Users> findByEmail(String email);
 }
